d7e0fd1d127585248255931e868735c97ddaf01e
Gennady <sorento_alba@list.ru>
2025-04-07 17:12:19 +0800

    may be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import os
 import kagglehub
 import keras
 from sklearn.model_selection import train_test_split
@@ -7,6 +8,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from turtle import pd
 import pandas
+import pickle
 
 from model import build_model
 
@@ -16,22 +18,48 @@ max_features = 10000
 maxlen = 500
 model_path = "models/RNNModel.keras"
 
-data = pandas.read_csv(dataset_path)
+tokenizer_path = 'tokenizer.pkl'
+train_data_path = 'train_data.pkl'
+test_data_path = 'test_data.pkl'
+
+data = pd.read_csv(dataset_path)
 
 x_train, x_test, y_train, y_test = train_test_split(data['text'], data['generated'], test_size=0.2, random_state=32)
 print("Train-test divided")
 
-tokenizer = Tokenizer(num_words=max_features)
-tokenizer.fit_on_texts(x_train)
-print("Tokenized")
+if os.path.exists(tokenizer_path):
+    print("Loading tokenizer from file...")
+    with open(tokenizer_path, 'rb') as f:
+        tokenizer = pickle.load(f)
+else:
+    print("Training tokenizer...")
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(x_train)
+    with open(tokenizer_path, 'wb') as f:
+        pickle.dump(tokenizer, f)
+    print("Tokenizer saved!")
 
-x_train_seq = tokenizer.texts_to_sequences(x_train)
-x_test_seq = tokenizer.texts_to_sequences(x_test)
-print("vectorizated or sequenced, idk")
+if os.path.exists(train_data_path) and os.path.exists(test_data_path):
+    print("Loading preprocessed data from file...")
+    with open(train_data_path, 'rb') as f:
+        x_train_pad, y_train = pickle.load(f)
+    with open(test_data_path, 'rb') as f:
+        x_test_pad, y_test = pickle.load(f)
+else:
+    print("Vectorizing and padding data...")
+    x_train_seq = tokenizer.texts_to_sequences(x_train)
+    x_test_seq = tokenizer.texts_to_sequences(x_test)
 
-x_train_pad = pad_sequences(x_train_seq, maxlen=100)
-x_test_pad = pad_sequences(x_test_seq, maxlen=100)
-print("padded")
+    x_train_pad = pad_sequences(x_train_seq, maxlen=100)
+    x_test_pad = pad_sequences(x_test_seq, maxlen=100)
+
+    with open(train_data_path, 'wb') as f:
+        pickle.dump((x_train_pad, y_train), f)
+    with open(test_data_path, 'wb') as f:
+        pickle.dump((x_test_pad, y_test), f)
+    print("Preprocessed data saved!")
+
+print("Data prepared.")
 
 callbacks = [
     ModelCheckpoint(
